9fd201ddc53d97a3e3c4436d8aea35e458b8f0d5
nepthius <anishdshekar@gmail.com>
2026-06-14 19:51:57 -0400

    fifth chapter

---
+++ chapter5/rectangles/src/main.rs
@@ -0,0 +1,19 @@
+struct Rectangle {
+    width: u32,
+    length: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width* self.length
+    }
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 30,
+        length: 50,
+    };
+
+    println!("area: {}", rect.area())
+}
